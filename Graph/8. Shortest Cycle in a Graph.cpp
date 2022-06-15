f7a1b51176e3cd9339dd28586cab42012a5df4d9
Emrul-Hasan-Emon <74479414+Emrul-Hasan-Emon@users.noreply.github.com>
2022-06-15 11:30:19 +0600

    Create 8. Shortest Cycle in a Graph.cpp

---
+++ Graph/8. Shortest Cycle in a Graph.cpp
@@ -0,0 +1,150 @@
+You are given a graph consists of nodes and edges. You have to say whether any cyle present in the graph or not. If present then print the length of minimum cycle.
+
+Problem: https://lightoj.com/problem/beehives
+
+///              B I S M I L L A H I R  R A H M A N I R  R A H I M
+
+// ============================================================================ //
+// ||                                                                        || //
+// ||             International University of Business Agriculture           || //
+// ||                    and Technology, Dhaka, Bangladesh                   || //
+// ||                           Emrul Hasan Emon                             || //
+// ||                                                                        || //
+// ============================================================================ //
+
+#include<bits/stdc++.h>
+#include<vector>
+#include<set>
+#include<map>
+#include<queue>
+using namespace std;
+typedef bool                      boo;
+typedef int                       li;
+typedef long long int             ll;
+typedef unsigned long long int    l1;
+typedef double                    db;
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+typedef vector < li >                   vli;
+typedef vector < ll >                   vll;
+typedef set < li >                      sli;
+typedef set < ll >                      sll;
+typedef pair < li, li >                pli;
+typedef pair < ll, li >                pll;
+typedef map < li,li >                  mli;
+typedef map < ll,ll >                  mll;
+typedef vector < pair < li, li > >     vpi;
+typedef vector < pair < ll, ll > >     vpl;
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#define tc                        int t;cin>>t;while(t--)
+#define inp_i(a,n)                for(i=0; i<n ;i++) cin>>a[i]
+#define out_i(a, b, c)            for(i=b; i<c; i++) cout<<a[i] spc; cout nl;
+#define lp(i,a,b)                 for(i=a;i<b;i++)
+#define len(z)                    z.begin(),z.end()
+#define faster                    ios::sync_with_stdio(0); cin.tie(0);cout.tie(0);
+#define input_txt                 freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#define nl                        <<"\n"
+#define spc                       <<" "
+#define sp                        <<" "<<
+#define co(x)                     cout<<x nl
+#define sz(a)                     a.size()
+#define cy                        cout<<"YES" nl
+#define cn                        cout<<"NO" nl
+#define pb                        push_back
+#define F                         first
+#define S                         second
+#define pi                        2*acos(0.0)
+#define clr(z)                    z.clear()
+#define rn                        return
+#define gcd(a,b)                  __gcd(a,b)
+#define mem(b,z)                  memset(b,z,sizeof(b))
+#define fixed(x)                  fixed<<setprecision(x)
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+const int lx = 5e3 + 123;
+const ll mod = 1e9+7;
+vector<int>adj[lx];
+bool vis[lx];
+int dis[lx];
+int p, cs = 1;
+
+int shortest_cycle(int n)
+{
+	int ans = INT_MAX;
+
+	for(int start = 1; start <= n; start++) {
+		for(int i = 0; i <= n+2; i++) {
+			vis[i] = false;
+			dis[i] = INT_MAX;
+		}
+		queue<pair<int, int > >q;
+		q.push({start, -1});
+		vis[start] = true;
+		dis[start] = 0;
+
+		while(!q.empty()) {
+			int node = q.front().first;
+			int parent = q.front().second;
+			q.pop();
+
+			for(int c : adj[node]) {
+				if(vis[c] == 0) {
+					vis[c] = true;
+					dis[c] = dis[node] + 1;
+					q.push({c, node});
+				} else {
+					if(c != parent) {
+						ans = min(ans, dis[node] + dis[c] + 1);
+					}
+				}
+			}
+		}
+	}
+	return ans;
+}
+
+void solve()
+{
+	int node, edge, m, i, f = 0, j;
+	cin >> node >> edge;
+	for(i = 0; i <= node+2; i++) adj[i].clear();
+
+	for(i = 1; i <= edge; i++) {
+		int u, v;
+		cin >> u >> v;
+
+		adj[u].push_back(v);
+		adj[v].push_back(u);
+
+	}
+
+	int mx;
+	mx = shortest_cycle(node);
+	if(mx == INT_MAX)
+        cout << "There is no cycle in the graph\n";
+    else
+        cout << "Length of the shortest cycle is " << mx << endl;
+}
+
+void init_code()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+}
+
+int main()
+{
+    faster
+
+    solve();
+
+// A L H A M D U L I L L A H
+// 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1 2 3 4 5....
+// Emrul Hasan Emon
+}
+
+
