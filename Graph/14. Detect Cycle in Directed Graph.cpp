62569ec07756a7ebd2682b0dbfb1539f7c4ed6ff
Emrul-Hasan-Emon <74479414+Emrul-Hasan-Emon@users.noreply.github.com>
2022-06-16 18:06:40 +0600

    Create 14. Detect Cycle in Directed Graph.cpp

---
+++ Graph/14. Detect Cycle in Directed Graph.cpp
@@ -0,0 +1,125 @@
+You are given directional graph consists of nodes and edges. You have to answer whether there is a cycle or not.
+  
+Code:
+
+///              B I S M I L L A H I R  R A H M A N I R  R A H I M
+
+// ============================================================================ //
+// ||                                                                        || //
+// ||             International University of Business Agriculture           || //
+// ||                    and Technology, Dhaka, Bangladesh                   || //
+// ||                           Emrul Hasan Emon                             || //
+// ||                                                                        || //
+// ============================================================================ //
+
+#include<bits/stdc++.h>
+#include<vector>
+#include<set>
+#include<map>
+#include<queue>
+using namespace std;
+typedef bool                      boo;
+typedef int                       li;
+typedef long long int             ll;
+typedef unsigned long long int    l1;
+typedef double                    db;
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+typedef vector < li >                   vli;
+typedef vector < ll >                   vll;
+typedef set < li >                      sli;
+typedef set < ll >                      sll;
+typedef pair < li, li >                pli;
+typedef pair < ll, li >                pll;
+typedef map < li,li >                  mli;
+typedef map < ll,ll >                  mll;
+typedef vector < pair < li, li > >     vpi;
+typedef vector < pair < ll, ll > >     vpl;
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#define tc                        int t;cin>>t;while(t--)
+#define inp_i(a,n)                for(i=0; i<n ;i++) cin>>a[i]
+#define out_i(a, b, c)            for(i=b; i<c; i++) cout<<a[i] spc; cout nl;
+#define lp(i,a,b)                 for(i=a;i<b;i++)
+#define len(z)                    z.begin(),z.end()
+#define faster                    ios::sync_with_stdio(0); cin.tie(0);cout.tie(0);
+#define input_txt                 freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#define nl                        <<"\n"
+#define spc                       <<" "
+#define sp                        <<" "<<
+#define co(x)                     cout<<x nl
+#define sz(a)                     a.size()
+#define cy                        cout<<"YES" nl
+#define cn                        cout<<"NO" nl
+#define pb                        push_back
+#define F                         first
+#define S                         second
+#define pi                        2*acos(0.0)
+#define clr(z)                    z.clear()
+#define rn                        return
+#define gcd(a,b)                  __gcd(a,b)
+#define mem(b,z)                  memset(b,z,sizeof(b))
+#define fixed(x)                  fixed<<setprecision(x)
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+const ll lx=2e6+7;
+const ll mod = 998244353;
+
+vector<int>adj[lx];
+bool vis[lx];
+bool dfsvis[lx];
+
+bool dfs_cycle(int node)
+{
+	vis[node] = true;
+	dfsvis[node] = true;
+	
+	for(int c : adj[node]) {
+		if(vis[c] == false) {
+			if(dfs_cycle(c) == true) return true;
+		} else if(dfsvis[c] == true) return true;
+	}
+	dfsvis[node] = false;
+	return false;
+}
+
+void solve()
+{
+	int node, edge, i;
+	cin >> node >> edge;
+	for(i = 1; i <= edge; i++) {
+		int u, v;
+		cin >> u >> v;
+		adj[u].push_back(v);
+	}    
+	bool flag = false;
+	for(i = 1; i <= node; i++) {
+		if(vis[i] == false) {
+			if(dfs_cycle(i) == true) flag = true;
+		}
+	}
+	if(flag == true) cout << "Cycle Detected\n";
+	else cout << "No Cycle\n";
+}
+
+void init_code()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+}
+
+int main()
+{
+    faster
+    
+    solve();
+
+// A L H A M D U L I L L A H
+// 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1 2 3 4 5....
+// Emrul Hasan Emon
+}
+
+
