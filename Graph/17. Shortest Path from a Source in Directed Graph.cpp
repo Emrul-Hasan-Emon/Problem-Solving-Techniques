412262cd16c0733242fc0bb93ebcb2b38aaa413c
Emrul-Hasan-Emon <74479414+Emrul-Hasan-Emon@users.noreply.github.com>
2022-06-18 10:37:07 +0600

    Create 17. Shortest Path from a Source in Directed Graph.cpp

---
+++ Graph/17. Shortest Path from a Source in Directed Graph.cpp
@@ -0,0 +1,148 @@
+The problem is I will be given a graph consists of nodes and edges. This will be a directed graph. And I will given a source also. I have to find the shortest distance
+from source to every other node.
+
+We can do this using topological sort technique. First we find the topological order. Then we can find the shortest distance using the topological order.
+
+///              B I S M I L L A H I R  R A H M A N I R  R A H I M
+
+// ============================================================================ //
+// ||                                                                        || //
+// ||             International University of Business Agriculture           || //
+// ||                    and Technology, Dhaka, Bangladesh                   || //
+// ||                           Emrul Hasan Emon                             || //
+// ||                                                                        || //
+// ============================================================================ //
+
+#include<bits/stdc++.h>
+#include<vector>
+#include<set>
+#include<map>
+#include<queue>
+using namespace std;
+typedef bool                      boo;
+typedef int                       li;
+typedef long long int             ll;
+typedef unsigned long long int    l1;
+typedef double                    db;
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+typedef vector < li >                   vli;
+typedef vector < ll >                   vll;
+typedef set < li >                      sli;
+typedef set < ll >                      sll;
+typedef pair < li, li >                pli;
+typedef pair < ll, li >                pll;
+typedef map < li,li >                  mli;
+typedef map < ll,ll >                  mll;
+typedef vector < pair < li, li > >     vpi;
+typedef vector < pair < ll, ll > >     vpl;
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#define tc                        int t;cin>>t;while(t--)
+#define inp_i(a,n)                for(i=0; i<n ;i++) cin>>a[i]
+#define out_i(a, b, c)            for(i=b; i<c; i++) cout<<a[i] spc; cout nl;
+#define lp(i,a,b)                 for(i=a;i<b;i++)
+#define len(z)                    z.begin(),z.end()
+#define faster                    ios::sync_with_stdio(0); cin.tie(0);cout.tie(0);
+#define input_txt                 freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#define nl                        <<"\n"
+#define spc                       <<" "
+#define sp                        <<" "<<
+#define co(x)                     cout<<x nl
+#define sz(a)                     a.size()
+#define cy                        cout<<"YES" nl
+#define cn                        cout<<"NO" nl
+#define pb                        push_back
+#define F                         first
+#define S                         second
+#define pi                        2*acos(0.0)
+#define clr(z)                    z.clear()
+#define rn                        return
+#define gcd(a,b)                  __gcd(a,b)
+#define mem(b,z)                  memset(b,z,sizeof(b))
+#define fixed(x)                  fixed<<setprecision(x)
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+const int lx=2e5+123;
+const ll mod = 1e9+7;
+
+vector<pair<int, int> >adj[lx];
+bool vis[lx];
+int dis[lx];
+stack<int>s;
+
+void dfs_topological_sort(int node) {
+	vis[node] = true;
+	for(auto it : adj[node]) {
+		int c = it.first;
+		if(vis[c] == false) {
+			dfs_topological_sort(c);
+		}
+	}
+	s.push(node);
+}
+
+void Find_Shortest_Distance(int source, int node)
+{
+	for(int i = 0; i <= node; i++) dis[i] = INT_MAX;
+	dis[source] = 0;
+	
+	while(!s.empty()) {
+		int x = s.top();
+		s.pop();
+		
+		if(dis[x] != INT_MAX) {
+			for(auto it : adj[x]) {
+				int _node = it.first;
+				int cost = it.second;
+				if(dis[x] + cost < dis[_node]) {
+					dis[_node] = dis[x] + cost;
+				}
+			}
+		}
+	}
+	for(int i = 1; i <= node; i++) cout << dis[i] << " "; cout << endl;
+}
+
+void solve()
+{
+	int node, edge, i;
+	cin >> node >> edge;
+	
+	for(i = 1; i <= edge; i++) {
+		int u, v, w;
+		cin >> u >> v >> w;
+		adj[u].push_back({v, w});
+	}
+	for(i = 0; i <= node+2; i++) {
+		vis[i] = false;
+	}
+	for(i = 1; i <= node; i++) {
+		if(vis[i] == false) {
+			dfs_topological_sort(i);
+		}
+	}
+	int source;
+	cin >> source;
+	Find_Shortest_Distance(1, node);
+}
+
+void init_code()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+}
+
+int main()
+{
+    faster
+    
+    solve();
+
+// A L H A M D U L I L L A H
+// 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1 2 3 4 5....
+// Emrul Hasan Emon
+}
